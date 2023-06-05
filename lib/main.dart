975ae28cc6d70a94e2150271a279e1073cdf8b3e
Alexander Mendivil <alexanderima@outlook.es>
2023-06-04 17:35:56 -0700

    lista de productos

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,11 @@ class MyApp extends StatelessWidget {
         'login': (_) => const LoginScreen(),
         'home': (_) => const HomeScreen(),
       },
-      theme: ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.grey[300]),
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Colors.grey[300], 
+        appBarTheme: const AppBarTheme(elevation: 0, color: Colors.indigo),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(backgroundColor: Colors.indigo, elevation: 0)
+        ),
       );
   }
 }
