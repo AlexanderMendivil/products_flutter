167f1856382ff58db4d69207e19204c184f97702
Alexander Mendivil <alexanderima@outlook.es>
2023-06-11 17:16:43 -0700

    env setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:products/screens/screens.dart';
 import 'package:products/services/services.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const AppState());
 }
 
--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../models/products.dart';
 
 class ProductsService extends ChangeNotifier{
-  final String _baseUrl = "flutter-tuto-e8ea4-default-rtdb.firebaseio.com";
+  final String _baseUrl = dotenv.get('BASE_URL');
 
   final List<Product> products = [];
 }
